2a95a1e092da5ebdd410a2565472f0f08a30ec5f
Jpdv9 <jean.davalos@correounivalle.edu.co>
2025-11-19 10:46:19 -0500

    Jean Davalos - InventoryService

--- demo_script.sh
+++ demo_script.sh
@@ -3,6 +3,7 @@ echo "Build images..."
 docker build -t online-store/checkout-service:dev ./1926462-checkout-service
 docker build -t online-store/order-validator:dev ./2159832-order-validator
 docker build -t online-store/shipping-service:dev ./2240581-shipping-service
+docker build -t online-store/inventory-service:dev ./1832375-inventory-service
 
 echo "Apply RabbitMQ..."
 kubectl apply -f k8s/rabbitmq-deployment.yaml
