d556a493e6eaf733cec8d4480bbfff80f6629e12
Juanmaperea <53235315+Juanmaperea@users.noreply.github.com>
2025-11-19 16:03:25 -0500

    Update demo_script.sh

--- demo_script.sh
+++ demo_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Build images..."
-docker build -t online-store/dashboard-service:dev ./2140132-dashboard-service
 docker build -t online-store/checkout-service:dev ./1926462-checkout-service
 docker build -t online-store/order-validator:dev ./2159832-order-validator
 docker build -t online-store/payment-service:dev ./2182527-payment-service
@@ -8,6 +7,7 @@ docker build -t online-store/inventory-service:dev ./1832375-inventory-service
 docker build -t online-store/shipping-service:dev ./2240581-shipping-service
 docker build -t online-store/order-history:dev ./2259395-order-history
 docker build -t online-store/notification-service:dev ./2179652-notification-service
+docker build -t online-store/dashboard-service:dev ./2140132-dashboard-service
 
 echo "Apply RabbitMQ..."
 kubectl apply -f k8s/rabbitmq-deployment.yaml
